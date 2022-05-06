5398930a221530c688c0007b5d1b3071040f2a84
crazy010323 <crazy010323@gmail.com>
2022-05-06 01:29:27 -0400

    added regex pattern for arguments

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 require('dotenv').config()
 
+const { randomInt } = require('crypto');
 const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {polling: true});
 
@@ -10,8 +11,12 @@ bot.onText(/\/callbackquery/, (msg, match) => {
             "inline_keyboard": [
                 [
                     {
-                        text: "Click me!",
-                        callback_data: "click",
+                      text: "Edit me!",
+                      callback_data: `/edit ${randomInt(100000, 999999)}`,
+                    },
+                    {
+                      text: "Reply to me!",
+                      callback_data: `/reply ${randomInt(100000, 999999)}`,
                     },
                 ],
             ],
@@ -21,22 +26,38 @@ bot.onText(/\/callbackquery/, (msg, match) => {
 
 
 bot.on("callback_query", (callbackQuery) => {
-    const msg = callbackQuery.message;
-    bot.editMessageText("New Edited msg", {
+    
+  const msg = callbackQuery.message;
+  const callbackData = callbackQuery.data;
+
+  if (callbackData.match(/\/edit [0-9]{6}/)) {
+    
+    const param = callbackData.split(' ')[1];
+    const  newText = `Edited message at ${param}`;
+
+    if (msg.text === newText) return;
+
+    bot.editMessageText(newText, {
       chat_id: msg.chat.id,
       message_id: msg.message_id,
       inline_message_id: callbackQuery.inline_message_id,
       reply_markup: {
         "inline_keyboard": [
             [
-                {
-                    text: "Click me!",
-                    callback_data: "click",
-                },
+              {
+                text: "Edit me!",
+                callback_data: `/edit ${randomInt(100000, 999999)}`,
+              },
+              {
+                text: "Reply to me!",
+                callback_data: `/reply ${randomInt(100000, 999999)}`,
+              },
             ],
         ],
-    },
+      },
     })
-    // bot.answerCallbackQuery(callbackQuery.id)
-    //     .then(() => bot.sendMessage(msg.chat.id, "You clicked!"));
+  } else if (callbackData.match(/\/reply/)) {
+    bot.answerCallbackQuery(callbackQuery.id)
+      .then(() => bot.sendMessage(msg.chat.id, "Here's the reply message!")); 
+  }
 });
